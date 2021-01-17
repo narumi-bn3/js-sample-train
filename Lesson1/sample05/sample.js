5157d9c7f2353a09f72932a852311ba2f608c294
narumi-bn3 <h4-toooooooo.01@docomo.ne.jp>
2021-01-17 12:09:51 +0900

    add Lesson1の文字列操作の練習の追加

---
+++ Lesson1/sample05/sample.js
@@ -0,0 +1,12 @@
+//文字列の操作
+const myFavorite1 = "カレー"
+const myFavorite2 = "ラーメン"
+//ブラウザのコンソール画面に出力
+console.log(myFavorite1 + "と" + myFavorite2)
+
+// +=えお使った文字列結合
+
+let myFavorite = "カレー"
+myFavorite += "と"
+myFavorite += "ラーメン"
+console.log(myFavorite)
