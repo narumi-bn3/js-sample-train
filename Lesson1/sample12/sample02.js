ca70185e3eaaa3859f9043031552c92670a290f2
narumi-bn3 <h4-toooooooo.01@docomo.ne.jp>
2021-01-17 13:15:40 +0900

    addif文の練習の追加

---
+++ Lesson1/sample12/sample02.js
@@ -0,0 +1,9 @@
+//値が等しいかどうかを確認
+const myString ="雨"
+const myBoolean = myString === "雨"
+console.log(myBoolean)
+
+
+console.log(myString === "曇り")
+
+console.log(myString !== "雨")
---
+++ Lesson1/sample12/sample03.js
@@ -0,0 +1,9 @@
+const myWeather = "雨"
+const isSunny = myWeather === "晴れ"
+if (myWeather === "晴れ"){
+    console.log("天気は晴れです。")
+}else if (myWeather === "雨"){
+    console.log("天気は雨です。")
+}else{
+    console.log("天気はわかりません。")
+}
